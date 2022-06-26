3c9d7bdfb278eb6441b883dfbcd2ba6153da1cd1
Abubakar Usman <108135762+Jrsubzee@users.noreply.github.com>
2022-06-26 13:56:30 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $dbhost = "0.0.0.0";
 $dbuser = "root";
 $dbpass = "root";
-$dbdb = "shafinmu";
+$dbdb = "newdb";
 $sql = mysqli_connect($dbhost, $dbuser, $dbpass, $dbdb);
 if (!$sql) {
     die('cant connect ' . mysqli_connect_errno());
